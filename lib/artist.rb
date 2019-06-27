e000e453e641fb82f5507dcc020f1c11bbf34c0d
Luke O'Malley <Lukeomalley95@gmail.com>
2019-06-27 09:01:26 -0400

    Completed lab 🤙

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,27 +1,29 @@
 class Artist
-  @@count = 0
+  @@song_count = 0
 
   def initialize(name)
     @name = name
     @songs = []
-    @@count += 1
+    
   end
 
-  attr_accessor :name, :songs, 
+  attr_accessor :name, :songs
 
   def add_song(song)
     @songs << song
     song.artist = self
+    @@song_count += 1
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
     song.artist = self
+    @@song_count += 1
   end
 
   def self.song_count
-    @@count
+    @@song_count
   end
 
 end
--- lib/author.rb
+++ lib/author.rb
@@ -1,25 +1,27 @@
 class Author
-  @@count = 0
+  @@post_count = 0
+
   def initialize(name)
     @name = name
     @posts = []
-    @@count += 1
   end
-
+  
   attr_accessor :name, :posts
-
+  
   def add_post(post)
     @posts << post
     post.author = self
+    @@post_count += 1
   end
 
   def add_post_by_title(title)
     post = Post.new(title)
     @posts << post
     post.author = self
+    @@post_count += 1
   end
 
   def self.post_count
-
+    @@post_count
   end
 end
